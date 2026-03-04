1ad7f5e3dbf686ee4006265ca01768f9800446f4
suyash <suygauri@gmail.com>
2026-03-04 13:04:27 +0530

    Home page
    Time stamp 2:08:15

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import Captainsignup from './pages/Captainsignup'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/UserLogin" element={<UserLogin />} />
+        <Route path="/UserSignup" element={<UserSignup />} />
+        <Route path="/CaptainLogin" element={<CaptainLogin />} />
+        <Route path="/Captainsignup" element={<Captainsignup />} />
+      </Routes>
     </div>
   )
 }
