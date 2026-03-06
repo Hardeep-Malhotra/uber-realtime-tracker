e91856923e2828d497f58bd2659afb949527dbe9
suyash <suygauri@gmail.com>
2026-03-06 12:05:42 +0530

    User Home Screen UI done
    time stamp 4:30:00

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from "../components/LocationSearchPanel";
 
-function Home() {
+const Home = () => {
+  const [pickup, setpickup] = useState("");
+  const [destination, setdestination] = useState("");
+  const [panaelOpen, setpanaelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null)
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useGSAP(
+    function () {
+      if (panaelOpen) {
+        gsap.to(panelRef.current, {
+          height: "70%",
+          padding:26
+          // opacity:1,
+        });
+        gsap.to(panelCloseRef.current,{
+          opacity:1
+        })
+      } else {
+        gsap.to(panelRef.current, {
+          height: "0%",
+          padding:0
+          // opacity:0
+
+        });
+        gsap.to(panelCloseRef.current,{
+          opacity:0
+        })
+      }
+    },
+    [panaelOpen],
+  );
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="h-screen relative">
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png"
+        alt="uber logo"
+      />
+      <div className="h-screen w-screen">
+        {/* image for temporary use */}
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt="uber map"
+        />
+      </div>
+      <div className="flex flex-col justify-end h-screen absolute top-0 w-full ">
+        <div className="h-[30%] bg-white p-6 relative">
+          <h5 ref={panelCloseRef} onClick={()=>{
+            setpanaelOpen(false)
+          }} className="absolute opacity-0 top-6 right-6 text-2xl">
+            <i className="ri-arrow-down-wide-line"></i>
+          </h5>
+          <h4 className="text-2xl font-semibold ">Find a Trip</h4>
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-700 rounded-full"></div>
+            <input
+              onClick={() => {
+                setpanaelOpen(true);
+              }}
+              value={pickup}
+              onChange={(e) => {
+                setpickup(e.target.value);
+              }}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Add a Pick-up location"
+            />
+            <input
+              onClick={() => {
+                setpanaelOpen(true);
+              }}
+              value={destination}
+              onChange={(e) => {
+                setdestination(e.target.value);
+              }}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3 "
+              type="text"
+              placeholder="Enter your destination"
+            />
+          </form>
+        </div>
+        <div ref={panelRef} className="h-[0%] bg-white ">
+              <LocationSearchPanel/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
